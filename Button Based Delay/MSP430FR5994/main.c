bd860ed6d0bc7033e7d4eabe830137d84f6f109f
Mitchell Hay <haym8@students.rowan.edu>
2017-10-11 17:33:02 -0400

    Added code for FR5994

---
+++ Button Based Delay/MSP430FR5994/main.c
@@ -0,0 +1,50 @@
+#include <msp430.h>
+
+int main(void) {
+	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+	volatile unsigned int i = 0;
+
+	// Disable the GPIO power-on default high-impedance mode
+	PM5CTL0 &= ~LOCKLPM5;
+
+	P1DIR |= BIT0;                            // P1.0 output
+	P1OUT &= BIT0;	// Initialize LED to 0
+
+	// Set up buttons
+	P5DIR &= ~BIT6;
+	P5OUT |= BIT6;
+	P5REN |= BIT6;
+
+	P5DIR &= ~BIT5;
+	P5OUT |= BIT5;
+	P5REN |= BIT5;
+
+	TA1CCTL0 = CCIE;                          // CCR0 interrupt enabled
+	TA1CCR0 = 3276;							// 10 Hz clock to sTArt
+	TA1CTL = TASSEL_1 + MC_1 + TACLR;         // ACLK, upmode, clear TAR
+
+	__bis_SR_register(GIE);       // Enter LPM3, enable interrupts
+	while (1) {
+		if ((P5IN & BIT6) != BIT6) {
+			i = 0;
+			while ((P5IN & BIT6) != BIT6) {
+				P1OUT |= BIT0;
+				i++;
+			}
+			TA1CCR0 = i;
+		}
+	}
+}
+
+// Timer1 A0 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void TIMER1_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER1_A0_VECTOR))) TIMER1_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+	P1OUT ^= 0x01;                            // Toggle P1.0
+}
---
+++ Button Interrupt/MSP430FR5994/main.c
@@ -0,0 +1,30 @@
+#include <msp430.h>
+
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+	// Disable the GPIO power-on default high-impedance mode
+	PM5CTL0 &= ~LOCKLPM5;
+  P1DIR |= BIT0;                            // Set P1.0 to output direction
+
+  P5IE |=  BIT6;                            // P2.1 interrupt enabled
+  P5IES |= BIT6;                            // P2.1 Hi/lo edge
+  P5REN |= BIT6;                            // Enable Pull Up on SW2 (P2.1)
+  P5IFG &= ~BIT6;                           // P2.1 IFG cleared
+
+  __bis_SR_register(LPM4_bits + GIE);       // Enter LPM4 w/interrupt
+}
+
+// Port 1 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=PORT5_VECTOR
+__interrupt void Port_5(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(PORT1_VECTOR))) Port_1 (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  P1OUT ^= BIT0;                            // P1.0 = toggle
+  P5IFG &= ~BIT6;                           // P2.1 IFG cleared
+}

25d11a1d640c316d8e7fa04b40e1b72f5851ab47
haym8 <31711187+haym8@users.noreply.github.com>
2017-10-11 20:52:50 -0400

    Update main.c

--- Button Based Delay/MSP430G2553/main.c
+++ Button Based Delay/MSP430G2553/main.c
@@ -1,11 +1,18 @@
+/*
+ * Mitchell Hay
+ * RU09342
+ * Lab 3 Button Based Delay
+ * MSP430G2553
+ */
+
 #include <msp430.h>
 
 int main(void) {
 	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-	volatile unsigned int i = 0;
+	volatile unsigned int i = 0;		  // Make counter
 
 	P1DIR |= BIT0;                            // P1.0 output
-	P1OUT &= BIT0;	// Initialize LED to 0
+	P1OUT &= BIT0;				  // Initialize LED to 0
 
 	// Set up button
 	P1DIR &= ~BIT3;
@@ -13,21 +20,23 @@ int main(void) {
 	P1REN |= BIT3;
 
 	CCTL0 = CCIE;                          // CCR0 interrupt enabled
-	CCR0 = 1200;							// 10 Hz clock to start
-	TACTL = TASSEL_1 + MC_1 + TACLR;         // ACLK, upmode, clear TAR
+	CCR0 = 1200;		               // 10 Hz clock to start
+	TACTL = TASSEL_1 + MC_1 + TACLR;       // ACLK, upmode, clear TAR
 
 	__bis_SR_register(GIE);       // Enter LPM3, enable interrupts
 	while (1) {
+		// If button is pressed
 		if ((P1IN & BIT3) != BIT3) {
 			i = 0;
+			// Count from 0 while button is pressed
 			while ((P1IN & BIT3) != BIT3) {
 				P1OUT |= BIT0;
 				i++;
 			}
+			// Assign count to CCR0
 			CCR0 = i;
 		}
 	}
-	//__no_operation();                         // For debugger
 }
 
 // Timer A0 interrupt service routine
